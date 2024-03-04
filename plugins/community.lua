bb5329979ba9f31083c2d6036adee321bbd2912d
lovelock <happyhackerwqc@foxmail.com>
2024-03-04 09:37:07 +0800

    添加pack，调整通知样式

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,14 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.php" },
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
