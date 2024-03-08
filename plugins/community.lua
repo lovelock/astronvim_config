b157c324f486a3653edd741b35d3fffa9d939e67
lovelock <happyhackerwqc@foxmail.com>
2024-03-08 21:47:54 +0800

    调整配置

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,10 +5,20 @@ return {
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.php" },
-  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.toml" },
+
+  { import = "astrocommunity.motion.mini-surround" },
+  { import = "astrocommunity.motion.flash-nvim" },
+
+  { import = "astrocommunity.search.nvim-hlslens" },
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.gruvbox-nvim", enabled = true },
+  { import = "astrocommunity.colorscheme.gruvbox-baby" },
+
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
