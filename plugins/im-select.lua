3083f13c43967411548b998f38b0afff7ecbb4c7
lovelock <happyhackerwqc@foxmail.com>
2024-03-20 16:54:01 +0800

    im-select更新

--- plugins/im-select.lua
+++ plugins/im-select.lua
@@ -1,6 +1,7 @@
 return {
   {
     "keaising/im-select.nvim",
+    lazy = false,
     config = function()
       require("im_select").setup {
         -- IM will be set to `default_im_select` in `normal` mode
@@ -26,8 +27,9 @@ return {
         -- Restore the previous used input method state when the following events
         -- are triggered, if you don't want to restore previous used im in Insert mode,
         -- e.g. deprecated `disable_auto_restore = 1`, just let it empty
+        set_previous_events = {},
         -- as `set_previous_events = {}`
-        set_previous_events = { "InsertEnter" },
+        -- set_previous_events = { "InsertEnter" },
 
         -- Show notification about how to install executable binary when binary missed
         keep_quiet_on_no_binary = false,
